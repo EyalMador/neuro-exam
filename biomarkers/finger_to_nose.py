58e081865fe0136ccd0c014fbba9e2591a0cdf29
EyalMador <eyal_m@live.com>
2025-09-20 12:13:31 +0300

    Update finger_to_nose.py

--- biomarkers/finger_to_nose.py
+++ biomarkers/finger_to_nose.py
@@ -1,7 +1,10 @@
 models = ["pose","hands"]
 
 def calculate_accuracy(datapoints):
-  pass
+  # find frame of local minimums in z values
+  # for each minimum, extract distance between nose and tip of index finger
+    # take into account distance from camera
+  # calculate overall biomarker for distance
 
 def calculate_smoothness(datapoints):
   pass
@@ -9,9 +12,10 @@ def calculate_smoothness(datapoints):
 def calculate_consistency(datapoints):
   pass
 
-def produce_finger_to_nose_biomarkers(datapoints):
+def extract_finger_to_nose_biomarkers(datapoints):
   biomarkers = {}
   biomarkers["accuracy"] = calculate_accuracy(datapoints)
   biomarkers["smoothness"] = calculate_smoothness(datapoints)
   biomarkers["consistency"] = calculate_consistency(datapoints)
   return biomarkers
+
