2ded9cf169dfdd0a2bfa2802b54af8dfaf60905b
EyalMador <eyal_m@live.com>
2025-10-21 21:34:59 +0300

    Update and rename scripts/temp to interface.py

---
+++ interface.py
@@ -0,0 +1,31 @@
+while True:
+  while True:
+    chosen_operation = input("Choose train/predict/exit: ")
+    if chosen_operation in ["train", "predict", "exit"]:
+      break
+
+  if chosen_operation == "train":
+    while True:
+      chosen_model = input("Enter requested model to train: ")
+      if chosen_model not in supported_models:
+        print(f"Error: {chosen_model} model not supported.")
+      else:
+        break
+    train_model(chosen_model)
+
+  if chosen_operation == "predict":
+    while True:
+      chosen_model = input("Enter requested model to predict: ")
+      if chosen_model not in supported_models:
+        print(f"Error: {chosen_model} model not supported.")
+      else:
+        break
+    filename = input("Enter name of data file: ")
+    try:
+      predict_result(chosen_model, filename)
+    except Exception as e:
+      print(e)
+      continue
+
+  if chosen_operation == "exit":
+    break
