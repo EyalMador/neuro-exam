4a3611f65bed9ff91d5cb1b82eefbd00b3d18444
EyalMador <eyal_m@live.com>
2025-10-21 22:50:26 +0300

    Update interface.py

--- scripts/interface.py
+++ scripts/interface.py
@@ -3,35 +3,36 @@ from IPython.display import clear_output
 
 supported_tests = ["straight_walk", "heel_to_toe_walk", "raise_hands", "finger_to_nose"]
 
-while True:
+def main():
   while True:
-    chosen_operation = input("Choose train/classify/exit: ")
-    if chosen_operation in ["train", "predict", "exit"]:
-      clear_output(wait=False)
+    while True:
+      chosen_operation = input("Choose train/classify/exit: ")
+      if chosen_operation in ["train", "predict", "exit"]:
+        clear_output(wait=False)
+        break
+  
+    if chosen_operation == "train":
+      while True:
+        chosen_test = input("Enter requested test to train: ")
+        if chosen_test not in supported_tests:
+          print(f"Error: {chosen_test} test not supported.")
+        else:
+          break
+      train_model(chosen_test)
+  
+    if chosen_operation == "classify":
+      while True:
+        chosen_test = input("Enter requested test to classify: ")
+        if chosen_test not in supported_tests:
+          print(f"Error: {chosen_test} test not supported.")
+        else:
+          break
+      filename = input("Enter name of video file: ")
+      try:
+        classify_video(chosen_test, filename)
+      except Exception as e:
+        print(e)
+        continue
+  
+    if chosen_operation == "exit":
       break
-
-  if chosen_operation == "train":
-    while True:
-      chosen_test = input("Enter requested test to train: ")
-      if chosen_test not in supported_tests:
-        print(f"Error: {chosen_test} test not supported.")
-      else:
-        break
-    train_model(chosen_test)
-
-  if chosen_operation == "classify":
-    while True:
-      chosen_test = input("Enter requested test to classify: ")
-      if chosen_test not in supported_tests:
-        print(f"Error: {chosen_test} test not supported.")
-      else:
-        break
-    filename = input("Enter name of video file: ")
-    try:
-      classify_video(chosen_test, filename)
-    except Exception as e:
-      print(e)
-      continue
-
-  if chosen_operation == "exit":
-    break
