814191760bdd92aae308261b998456db5661c007
EyalMador <eyal_m@live.com>
2025-10-21 21:33:07 +0300

    Update automation.py

--- scripts/automation.py
+++ scripts/automation.py
@@ -1,1 +1,20 @@
+#insert initialization scripts - drive mount, git clone, pip installs
+#insert clear command to remove prints
 
+def classify_test(test_type, video_name):
+  print("Starting classification process.")
+  create_temp_folder()
+  extract_landmarks(test_type, video_name)
+  calculate_biomarkers(test_type)
+  predict_result(test_type)
+  cleanup_folder()
+  print("Classification process finished.")
+
+def train_model(test_type):
+  print("Starting training process.")
+  create_temp_folder()
+  extract_landmarks(test_type)
+  calculate_biomarkers(test_type)
+  train_svm(test_type)
+  cleanup_folder()
+  print("Training process finished.")
